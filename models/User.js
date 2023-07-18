f76d76b6dadd49d37b96de86d8915e02deec34ac
ckc2007 <ckc2007@gmail.com>
2023-07-18 19:44:57 -0400

    created User model

--- models/User.js
+++ models/User.js
@@ -1,33 +1,51 @@
-const { Schema, model } = require('mongoose');
-const assignmentSchema = require('./Thought');
+const { Schema, model } = require("mongoose");
 
-// Schema to create Student model
-const studentSchema = new Schema(
+// Schema to create User model
+const userSchema = new Schema(
   {
-    first: {
+    username: {
       type: String,
       required: true,
+      unique: true,
       max_length: 50,
+      trimmed: true,
     },
-    last: {
+    email: {
       type: String,
       required: true,
+      unique: true,
       max_length: 50,
     },
-    github: {
-      type: String,
-      required: true,
-      max_length: 50,
-    },
-    assignments: [assignmentSchema],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     toJSON: {
-      getters: true,
+      virtuals: true,
     },
+    id: false,
   }
 );
 
-const Student = model('student', studentSchema);
+// A virtual property is a property that is not stored directly in the database but is calculated or derived from other properties.
+// The .virtual() method is a Mongoose method used to define virtual properties for a schema
+// The virtual property is useful for situations where you need to calculate or derive a property value on the fly based on other data in the document without storing it redundantly in the database.
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-module.exports = Student;
+const User = model("user", userSchema);
+
+module.exports = User;
+
+// refactor this
