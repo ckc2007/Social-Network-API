7ef8b171519c3f35ba4896c630ea3d484febf9e8
ckc2007 <ckc2007@gmail.com>
2023-07-18 20:30:20 -0400

    setting up server with more error handling

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+// const express = require("express");
+// const db = require("./config/connection");
+// const routes = require("./routes");
+
+// const cwd = process.cwd();
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+// // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
+// const activity = cwd.includes("Social-Network-API")
+//   ? cwd.split("/Social-Network-API/")[1]
+//   : cwd;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(routes);
+
+// db.once("open", () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server for ${activity} running on port ${PORT}!`);
+//   });
+// });
+
 const express = require("express");
-const db = require("./config/connection");
+const connectDB = require("./config/db");
 const routes = require("./routes");
 
 const cwd = process.cwd();
@@ -16,8 +40,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
-  });
+// Add error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+// Connect to MongoDB and start the server
+connectDB(); // This will invoke the connectDB function in db.js
+
+app.listen(PORT, () => {
+  console.log(`API server for ${activity} running on port ${PORT}!`);
 });
